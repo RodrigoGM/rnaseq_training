b2c8ef160871e52c8b10524a83d9b1101bf0906c
Rodrigo Gularte Merida <gularter@mskcc.org>
2022-09-07 09:11:19 -0400

    adding vignettes, expanding, and debugging code

---
+++ data-raw/DATASET.R
@@ -0,0 +1,3 @@
+## Single-cell transcriptomes of the human skin reveal age-related loss of fibroblast priming
+## Reanalysis of the data from Llorenç Solé-Boldo, et al., & Frank Lyko
+## Gene read count matrix both raw and filtered are under accession GSE130973, at https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE130973 .
